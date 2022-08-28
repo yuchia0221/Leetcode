a2f382917aea87af9c87c8134b77252e3a12c4a6
Yu-Chia Chen <yuchiachen99@gmail.com>
2022-08-28 10:52:12 -0500

    Complete Leetcode 252

---
+++ Array & Hashing/252-MeetingRooms/solution.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        intervals.sort()
+        for i in range(1, len(intervals)):
+            if intervals[i][0] < intervals[i-1][1]:
+                return False
+        return True
