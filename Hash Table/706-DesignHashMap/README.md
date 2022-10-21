47ef53e1badb421ca930e87986623bd8c02a9915
Yu-Chia Chen <yuchiachen99@gmail.com>
2022-10-21 14:23:07 -0500

    Complete Leetcode 706

---
+++ Hash Table/706-DesignHashMap/README.md
@@ -0,0 +1,7 @@
+# Design HashMap
+
+Problem can be found in [here](https://leetcode.com/problems/design-hashmap/)!
+
+### [Solution](/Hash%20Table/706-DesignHashMap/solution.py): Hash Table
+
+Time Complexity: ![O(1)](<https://latex.codecogs.com/svg.image?\inline&space;O(1)>), Space Complexity: ![O(n)](<https://latex.codecogs.com/svg.image?\inline&space;O(n)>)
---
+++ Hash Table/706-DesignHashMap/solution.py
@@ -0,0 +1,51 @@
+class ListNode:
+    def __init__(self, key: int, val: int):
+        self.pair = (key, val)
+        self.next = None
+
+
+class MyHashMap:
+    def __init__(self):
+        self.m = 2287
+        self.h = [None]*self.m
+
+    def put(self, key: int, value: int):
+        index = key % self.m
+        if self.h[index] == None:
+            self.h[index] = ListNode(key, value)
+        else:
+            cur = self.h[index]
+            while True:
+                if cur.pair[0] == key:
+                    cur.pair = (key, value)  # update
+                    return
+                if cur.next == None:
+                    break
+                cur = cur.next
+            cur.next = ListNode(key, value)
+
+    def get(self, key: int):
+        index = key % self.m
+        cur = self.h[index]
+        while cur:
+            if cur.pair[0] == key:
+                return cur.pair[1]
+            else:
+                cur = cur.next
+        return -1
+
+    def remove(self, key: int):
+        index = key % self.m
+        cur = prev = self.h[index]
+        if not cur:
+            return
+        if cur.pair[0] == key:
+            self.h[index] = cur.next
+        else:
+            cur = cur.next
+            while cur:
+                if cur.pair[0] == key:
+                    prev.next = cur.next
+                    break
+                else:
+                    cur, prev = cur.next, prev.next
