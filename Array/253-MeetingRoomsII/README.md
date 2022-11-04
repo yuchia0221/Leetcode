23384fa226931c1890799bc12eeadb3a785e3058
Yu-Chia Chen <yuchiachen99@gmail.com>
2022-11-04 16:19:48 -0500

    Update Solution for Leetcode #253

--- Array/253-MeetingRoomsII/README.md
+++ Array/253-MeetingRoomsII/README.md
@@ -5,18 +5,17 @@ Problem can be found in [here](https://leetcode.com/problems/meeting-rooms-ii)!
 ### [Solution](/Array/252-MeetingRooms/solution.py)
 
 ```python
-def min_meeting_rooms(intervals: List[List[int]]) -> int:
-    new_intervals = [j for i in intervals for j in [[i[0], 1], [i[0], -1]]]
+def minMeetingRooms(intervals: List[List[int]]) -> int:
+    new_intervals = []
+    for interval in intervals:
+        new_intervals.append([interval[0], 1])
+        new_intervals.append([interval[1], -1])
+
     new_intervals.sort()
 
-    start_time = end_time = max_overlap = counter = 0
-    for time, room_needed in new_intervals:
-        if max_overlap > counter:
-            if room_needed == 1:
-                start_time = time
-            else:
-                end_time = time
-        counter += room_needed
+    counter = max_overlap = 0
+    for _, isMeetingStart in new_intervals:
+        counter += isMeetingStart
         max_overlap = max(max_overlap, counter)
 
     return max_overlap
--- Array/253-MeetingRoomsII/solution.py
+++ Array/253-MeetingRoomsII/solution.py
@@ -2,18 +2,17 @@ from typing import List
 
 
 class Solution:
-    def min_meeting_rooms(self, intervals: List[List[int]]) -> int:
-        new_intervals = [j for i in intervals for j in [[i[0], 1], [i[0], -1]]]
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        new_intervals = []
+        for interval in intervals:
+            new_intervals.append([interval[0], 1])
+            new_intervals.append([interval[1], -1])
+
         new_intervals.sort()
 
-        start_time = end_time = max_overlap = counter = 0
-        for time, room_needed in new_intervals:
-            if max_overlap > counter:
-                if room_needed == 1:
-                    start_time = time
-                else:
-                    end_time = time
-            counter += room_needed
+        counter = max_overlap = 0
+        for _, isMeetingStart in new_intervals:
+            counter += isMeetingStart
             max_overlap = max(max_overlap, counter)
-        
+
         return max_overlap
